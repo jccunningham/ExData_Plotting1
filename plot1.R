21f54ba366ac4e767531a5f756e965d05bae19c4
Juliette Cunningham <juliette.c.cunningham@gmail.com>
2016-02-15 04:18:08 +0100

    plot files and scripts added

---
+++ plot1.R
@@ -0,0 +1,35 @@
+#SOURCE DATA
+# To run this script please dowload this zip archive: 
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# and extract the household_power_consumption.txt data file to your working directory 
+
+#READ DATA SAMPLE FROM SOURCE
+#retrieve column names from the data source  
+sample<- read.table(file="household_power_consumption.txt",nrows=100, sep=";")
+
+names(sample)
+cnames<-lapply(sample[1,], as.character)
+
+#read the data into R
+classes<-c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+hpc<- read.table(file="household_power_consumption.txt",col.names = cnames,
+                 skip=1,  sep=";", stringsAsFactors=FALSE,na.strings = "?")
+
+#add a column combining Date and Time values in a datetime format
+hpc$DateTime <- strptime(paste(hpc$Date, " ", hpc$Time), "%d/%m/%Y %H:%M:%S")
+
+
+hpc<-as.data.frame.matrix(hpc) 
+
+#select only rows dated 1st or 2nd February 2007 
+daterange<-hpc$DateTime>=as.POSIXct("2007-02-01")&hpc$DateTime<as.POSIXct("2007-02-03")&is.na(hpc$DateTime)==FALSE
+plotdata <- hpc[daterange, ]
+
+#CREATE PLOT
+#open 480x480 pixel canvas
+windows(width=6.4, height=6.4, bg="white")
+
+#create histogram of Global Active Power for sample dates and copy to a png file
+hist(plotdata[,3], main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+dev.print(png, file="plot1.png", width=480, height=480)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,55 @@
+#SOURCE DATA
+# To run this script please dowload this zip archive: 
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# and extract the household_power_consumption.txt data file to your working directory 
+
+#READ DATA SAMPLE FROM SOURCE
+#retrieve column names from the data source  
+sample<- read.table(file="household_power_consumption.txt",nrows=100, sep=";")
+
+names(sample)
+cnames<-lapply(sample[1,], as.character)
+
+#read the data into R
+classes<-c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+hpc<- read.table(file="household_power_consumption.txt",col.names = cnames,
+                 skip=1,  sep=";", stringsAsFactors=FALSE,na.strings = "?")
+
+#add a column combining Date and Time values in a datetime format
+hpc$DateTime <- strptime(paste(hpc$Date, " ", hpc$Time), "%d/%m/%Y %H:%M:%S")
+
+
+hpc<-as.data.frame.matrix(hpc) 
+
+#select only rows dated 1st or 2nd February 2007 
+daterange<-hpc$DateTime>=as.POSIXct("2007-02-01")&hpc$DateTime<as.POSIXct("2007-02-03")&is.na(hpc$DateTime)==FALSE
+plotdata <- hpc[daterange, ]
+
+#CREATE PLOT CANVAS
+#open 480x480 pixel canvas
+
+png(file="plot4.png", units="px", width=480, height=480)
+
+par(mfrow=c(2,2))
+
+#Global Active Power  timeline
+plot(plotdata$DateTime, plotdata[,3], type="l",  ylab="Global Active Power (kilowatts)", xlab="")
+
+#Voltage timeline
+plot(plotdata$DateTime, plotdata$Voltage, type="l",  ylab="Voltage", xlab="datetime")
+
+#Sub_metering by Date/Time
+plot(plotdata$DateTime, plotdata$Sub_metering_1, type="n",  ylab="Energy sub metering", xlab="")
+lines(plotdata$DateTime, plotdata$Sub_metering_1)
+lines(plotdata$DateTime, plotdata$Sub_metering_2, col="red")
+lines(plotdata$DateTime, plotdata$Sub_metering_3, col="blue")
+legend(x="topright", 
+      legend= c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+      col=c("black", "blue", "red"),
+      lwd=1,
+      lty=c(1,1,1),
+      cex=0.75)
+
+#Global Reactive Power  timeline
+plot(plotdata$DateTime, plotdata[,4], type="l",  ylab="Global_reactive_power", xlab="datetime")
+dev.off()
